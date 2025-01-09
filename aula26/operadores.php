d5baba7d682f32c7d9b5b3c66913439e311f11d9
“Charles <charlesp.foripa@gmail.com>
2025-01-09 17:40:52 -0300

    Aula 26

---
+++ aula26/operadores.php
@@ -0,0 +1,35 @@
+<?php
+echo "<h1>Operações Aritméticas</h1>";
+
+echo "<h2>Qual o resultado de \"5\" + \"2\" ?</h2>";
+$r = "5" + "2";
+print("Resultado: $r");
+var_dump($r);
+
+echo "<h2>Operador de Adição</h2>";
+$r = 5 + 2;
+print("Resultado: $r");
+
+echo "<h2>Operador de Subtração</h2>";
+$r = 5 - 2;
+print("Resultado: $r");
+
+echo "<h2>Operador de Multiplicação</h2>";
+$r = 5 * 2;
+print("Resultado: $r");
+
+echo "<h2>Operador de Divisão</h2>";
+$r = 5 / 2;
+print("Resultado: $r");
+
+echo "<h2>Operador Módulo ou Resto da Divisão</h2>";
+$r = 5 % 2;
+print("Resultado: $r");
+
+echo "<h2>Operador de Potenciação</h2>";
+$r = 5 ** 2;
+print("Resultado: $r");
+
+echo "<h2>Para a Linguagem PHP, a expressão 50 / 2 + 3 **2 tem como resultado?</h2>";
+$r = 50 / 2 + 3 **2;
+print("Resultado: $r");
