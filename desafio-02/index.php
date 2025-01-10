6443286e9bb8b7214ecaee3feaa86e99a7c363ca
“Charles <charlesp.foripa@gmail.com>
2025-01-10 00:12:39 -0300

    Desafio03 - corrigido

--- desafio-02/index.php
+++ desafio-02/index.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado</title>
+    <title>Desafio PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <main>
     <h1>Trabalhando com números aleatórios</h1>
     <?php
-    $aleatorio = mt_rand(0, 100);
+    $min = 0;
+    $max = 100;
+    $aleatorio = mt_rand($min, $max);
     // rand() = 1951 - Linear Congrential Generator
     // Lenta nas versões abaixo da v7
     // mt_rand() - 1997 - Mersenne Twister (recomendada)
     // random_int() - gera números aleatórios criptograficamente seguros (lento)
 
-    print("Gerando número aleatório entre 0 e 100");
-    print("<br>O valor gerado foi: $aleatorio ");
+    print("Gerando número aleatório entre $min e $max <br>O valor gerado foi: <strong>$aleatorio</strong>");
 
     ?>
-    <br><br>
-    <input type="submit" value="Gerar outro">
+    <button onclick="javascript:document.location.reload()">&#x1F504; Gerar número Aleatório</button>
 </main>
 </body>
 </html>
--- desafio-03/app.php
+++ desafio-03/app.php
@@ -13,16 +13,24 @@
 <main>
     <h1>Conversor de Moedas v1.0</h1>
     <?php
+    // Cotação manual
+    $cotacao = 5.17;
     $reais = $_GET["num"];
-    $dolar = $reais * 5.22;
+    $dolar = $reais / $cotacao;
 
+    // Mostra o resultado
+    //print("Seus R$ ". number_format($reais, 2, ",", "."). " equivalem a US$". number_format($dolar, 2, ".", ","));
 
-    print("Seus R$ $reais equivalem a US$ $dolar");
+    // Formatação de moedas com internacionalização!
+    // Biblioteca intl (internacionalization PHP)
+    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
-    print("<br><br><strong>*Cotação fixa de R$ 5,22</strong> informada diretamente no código.");
+    echo "<p>Seus ". numfmt_format_currency($padrao, $reais, "BRL"). " equivalem a  <strong>". numfmt_format_currency($padrao, $dolar, "USD"). "</strong></p>";
+
+    echo "<br><strong>*Cotação fixa de R\$ $cotacao</strong> informada diretamente no código.";
 
     ?>
-    <p><a href="javascript:history.go(-1)">Voltar</a></p>
+    <button onclick="javascript:history.back()">Voltar</button>
 
 </main>
 </body>
